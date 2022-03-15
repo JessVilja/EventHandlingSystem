0690e7107f408ff037df0f86716f6281ef6b15af
Jessica Söderback <jessicavilja@gmail.com>
2022-03-15 10:16:38 +0100

    Skapat mappar och filstruktur

---
+++ EventiaWebapp/Controllers/EventController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EventiaWebapp.Controllers
+{
+    public class EventController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Events()
+        {
+            return View();
+        }
+
+        public IActionResult MyEvents()
+        {
+            return View();
+        }
+    }
+}
